96c46b7e5558c5d49555e4bf835fde07d65e61c8
Giovanni <giocornejo424@gmail.com>
2021-12-17 07:48:08 -0500

    Began Main Function and ZipExtract

---
+++ LetterboxdOrganizer.py
@@ -0,0 +1,38 @@
+# Python Modules
+from Download import download
+from ZipExtract import zip_extract
+
+# Import Internal Libraries
+import os
+import shutil
+from urllib.parse import urlparse
+from getpass import getpass
+
+if __name__ == "__main__":
+    while(True):
+        # Constants
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        DATA_DIR = os.path.join(BASE_DIR, "data")
+        # If Data Directory already has values
+        if os.path.exists(DATA_DIR):
+            shutil.rmtree(DATA_DIR)
+        os.makedirs(DATA_DIR)
+        
+        # Get Username and Password
+        username = getpass("Input your Letterboxd username\n")
+        password = getpass("Input your Letterboxd password\n")
+
+        # Download Stats Zip File
+        dl_pass = download(username, password)
+        if(not dl_pass):
+            print("Invalid Username or Password\n")
+            continue
+
+        # Extract Lists from Zip File into Data Folder
+        zip_pass = zip_extract()
+        if(not zip_extract):
+            print("Error when Zip Extracting!")
+
+        """Continue From Here..."""
+
+        break
---
+++ ZipExtract.py
@@ -0,0 +1,16 @@
+# Internal Libraries for Zip Extract
+import os
+from zipfile import ZipFile
+
+def zip_extract():
+    # Determine Directory of ZipFile
+    zip_response = input("Is your zip file in your Downloads folder (Y/N): ")
+    
+    """Continue From Here..."""
+
+
+    #zip_file = ZipFile(ZIP_DUR)
+
+    return True
+
+zip_extract()
